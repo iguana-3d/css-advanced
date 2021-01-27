87240b6497c6acae8a5f9a50cd3078f6e688dd59
Igor Chaves <igor.chaves@lufthansacc.com>
2021-01-27 15:31:21 -0300

    landpage

--- src/pages/header.js
+++ src/pages/header.js
@@ -4,7 +4,15 @@ import { HeaderStyle } from '../../styles/headerStyle';
 export default function Header(){
     return(
         <HeaderStyle>
-            AAAAAAAAAAAAAAAAAAA
+            <div className="logo-box">
+                <img src="/static/images/logo-white.png" alt="logo" className="logo" />
+            </div>
+            <div className="text-box">
+                <h1 className="heading-primary">
+                    <span className="heading-primary-main">Outdoors</span>
+                    <span className="heading-primary-sub">is where life happens</span>
+                </h1>
+            </div>            
         </HeaderStyle>
     )
 }
