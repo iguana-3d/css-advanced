58d402c8765fd097bb2e7b1ab1d2c470aba2cb32
Igor Chaves <igor.chaves@lufthansacc.com>
2021-02-02 15:21:21 -0300

    card effect

--- src/pages/header.js
+++ src/pages/header.js
@@ -97,6 +97,31 @@ export default function Header(){
                         </div>
                     </div>
                 </section>
+                <section className="section-tours">
+                    <div className="u-center-text u-margin-bottom-big">
+                        <h2 className="heading-secondary">
+                            Most popular tours
+                        </h2>
+                    </div>
+                    <div className="row">
+                        <div className="col-1-of-3">
+                            <div className="card">
+                                <div className="card__side card__side--front">
+                                    Front
+                                </div>
+                                <div className="card__side card__side--back card__side--back-1">
+                                    Back
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-1-of-3">
+                            col 1 of 3
+                        </div>
+                        <div className="col-1-of-3">
+                            col 1 of 3
+                        </div>
+                    </div>
+                </section>
             </main>
 
             {/* <section className="grid-test">
