41d808b0396d568efc12feeba052a96f07d767f3
Igor Chaves <igor.chaves@lufthansacc.com>
2021-02-02 11:23:08 -0300

    section 3

--- src/pages/header.js
+++ src/pages/header.js
@@ -62,6 +62,39 @@ export default function Header(){
                                     </p>
                             </div>
                         </div>
+                        <div className="col-1-of-4">
+                            <div className="feature-box">
+                                <i className="feature-box__icon icon-basic-compass"></i>
+                                    <h3 className="heading-tertiary u-margin-bottom-small">
+                                        Meet nature
+                                    </h3>
+                                    <p className="feature-box__text">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore totam
+                                    </p>
+                            </div>
+                        </div>
+                        <div className="col-1-of-4">
+                            <div className="feature-box">
+                                <i className="feature-box__icon icon-basic-map"></i>
+                                    <h3 className="heading-tertiary u-margin-bottom-small">
+                                        Find your way
+                                    </h3>
+                                    <p className="feature-box__text">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore totam
+                                    </p>
+                            </div>
+                        </div>
+                        <div className="col-1-of-4">
+                            <div className="feature-box">
+                                <i className="feature-box__icon icon-basic-heart"></i>
+                                    <h3 className="heading-tertiary u-margin-bottom-small">
+                                        Live a healthier life
+                                    </h3>
+                                    <p className="feature-box__text">
+                                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore totam
+                                    </p>
+                            </div>
+                        </div>
                     </div>
                 </section>
             </main>
