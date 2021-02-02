cdb8778ce42c30eb1bde25e32e9a0e4267d67b86
Igor Chaves <igor.chaves@lufthansacc.com>
2021-02-02 17:35:23 -0300

    card styling pt 2

--- src/pages/header.js
+++ src/pages/header.js
@@ -107,7 +107,17 @@ export default function Header(){
                         <div className="col-1-of-3">
                             <div className="card">
                                 <div className="card__side card__side--front">
-                                    Front
+                                    <div className="card__picture card__picture--1">
+                                        &nbsp;
+                                    </div>
+                                    <h4 className="card__heading">
+                                        <span className="card__heading-span card__heading-span--1">
+                                            The sea explorer
+                                        </span>                                        
+                                    </h4>
+                                    <div className="card__details">
+                                        Details
+                                    </div>
                                 </div>
                                 <div className="card__side card__side--back card__side--back-1">
                                     Back
