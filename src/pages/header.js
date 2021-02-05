b5e86235a11eab766feae8b21522f179cf740ac3
Igor Chaves <igor.chaves@lufthansacc.com>
2021-02-05 10:00:43 -0300

    Testimonials pt2

--- src/pages/header.js
+++ src/pages/header.js
@@ -205,8 +205,53 @@ export default function Header(){
                         <a href="#" className="btn btn--green">Discover all tours</a>
                     </div>
                 </section>
+                <section className="section-stories">
+                    <div className="u-center-text u-margin-bottom-big">
+                        <h2 className="heading-secondary">
+                            We make people genuinely happy
+                        </h2>
+                    </div>
+                    <div className="row">
+                        <div className="story">
+                            <figure className="story__shape">
+                                <img src="/static/images/nat-8.jpg" alt="Person on a tour" className="story__img" />
+                                <figcaption className="story__caption">
+                                    Mary Smith
+                                </figcaption>
+                            </figure>
+                            <div className="story__text">
+                                <h3 className="heading-tertiary u-margin-bottom-small">
+                                    I had the best week ever with my family
+                                </h3>
+                                <p>
+                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex obcaecati provident quidem deserunt ipsum in porro distinctio mollitia ab rem atque cumque velit culpa adipisci labore, accusantium omnis, similique saepe!
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="story">
+                            <figure className="story__shape">
+                                <img src="/static/images/nat-9.jpg" alt="Person on a tour" className="story__img" />
+                                <figcaption className="story__caption">
+                                    Jack Wilson
+                                </figcaption>
+                            </figure>
+                            <div className="story__text">
+                                <h3 className="heading-tertiary u-margin-bottom-small">
+                                    WOW! My life is complete difference now
+                                </h3>
+                                <p>
+                                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex obcaecati provident quidem deserunt ipsum in porro distinctio mollitia ab rem atque cumque velit culpa adipisci labore, accusantium omnis, similique saepe!
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="u-center-text u-margin-top-huge">
+                        <a href="#" className="btn-text">Read all stories &rarr;</a>
+                    </div>
+                </section>
             </main>
-
             {/* <section className="grid-test">
                 <div className="row">
                     <div className="col-1-of-2">
