1e2d5985d905b245f7ad141fae24de344342c394
Igor Chaves <igor.chaves@lufthansacc.com>
2021-02-06 14:04:50 -0300

    radio button

--- src/pages/sectionBook.js
+++ src/pages/sectionBook.js
@@ -20,6 +20,22 @@ export default function SectionBook(){
                                 <input type="email" className="form__input" id="email" placeholder="Email address" required/>
                                 <label htmlFor="email" className="form__label">Email address</label>    
                             </div>
+                            <div className="form__group">
+                                <div className="form__radio-group">
+                                    <input type="radio" className="form__radio-input" id="small" name="size" />
+                                    <label htmlFor="small" className="form__radio-label">
+                                        <span className="form__radio-button"></span>
+                                        Small tour group
+                                        </label>
+                                </div>
+                                <div className="form__radio-group">
+                                    <input type="radio" className="form__radio-input" id="large" name="size"/>
+                                    <label htmlFor="large" className="form__radio-label">
+                                        <span className="form__radio-button"></span>
+                                        Large tour group
+                                    </label>
+                                </div>
+                            </div>
                         </form>
                     </div>
                 </div>
