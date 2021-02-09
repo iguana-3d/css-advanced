c91527326ec8224d2c550ee4315038a1482388cc
Igor Chaves <igor.chaves@lufthansacc.com>
2021-02-09 16:26:13 -0300

    menu css

--- src/pages/navigation.js
+++ src/pages/navigation.js
@@ -4,7 +4,9 @@ export default function Navigation(){
     return(
         <div className="navigation">
             <input type="checkbox" className="navigation__checkbox" id="navi-toggle" />
-            <label htmlFor="navi-toggle" className="navigation__button">Menu</label>
+            <label htmlFor="navi-toggle" className="navigation__button">
+                <span className="navigation__icon">&nbsp;</span>
+            </label>
             <div className="navigation__background">&nbsp;</div>
             <nav className="navigation__nav">
                 <ul className="navigation__list">
